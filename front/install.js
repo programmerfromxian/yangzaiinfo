ae39eee47919320d391e84b6d31c386ff0f078d5
yangyang <1143733488@qq.com>
2020-05-26 00:42:49 +0800

    init

--- front/install.js
+++ front/install.js
@@ -18,7 +18,37 @@ new Vue({
             param1: "",
             param2: "",
             param3: ""
-        }
+        },
+        installInfo1: {
+            ip: "",
+            port: "22",
+            username: "root",
+            password: "",
+            ip1: "",
+            port1: "",
+            ip2: "",
+            port2: "",
+            id: "",
+            sftpUsername: "root",
+            sftpPassword: "",
+            filePath: "C:\Users\Administrator\Downloads\log.tar.gz",
+            param1: "",
+            param2: "",
+            param3: ""
+        },
+        period: {
+
+        },
+        options: [
+            {
+              value: 'option1',
+              label: 'option1'
+            }, {
+              value: 'option2',
+              label: 'option2'
+            }
+         ],
+         option: 'option1'
     },
     created: function () {
     },
@@ -37,6 +67,20 @@ new Vue({
                         this.$message.error(result.data.errMsg)
                     }
                 })
-        }
+        },
+        handleInstall1: function () {
+                    axios({
+                        method: "POST",
+                        url: URL.INSTALL.INSTALL,
+                        data: this.installInfo,
+                    })
+                        .then(result => {
+                            if (result.data.success === true) {
+                                this.$message.success(result.data.returnData)
+                            } else {
+                                this.$message.error(result.data.errMsg)
+                            }
+                        })
+                },
     }
 })
